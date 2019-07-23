9b967a59d72e422033a39b40c1b788298d1030a9
Flavio Leite <flaviorcleite@gmail.com>
2019-07-23 13:34:08 +0200

    Navegador implementado

--- iOS-WebBrowser/ViewController.swift
+++ iOS-WebBrowser/ViewController.swift
@@ -21,6 +21,8 @@ class ViewController: UIViewController {
     private let searchBar = UISearchBar()
     private var webView: WKWebView!
     private let refreshControl = UIRefreshControl()
+    private let baseURL = "http://www.google.com"
+    private let searchPath = "/search?q="
 
 
     override func viewDidLoad() {
@@ -43,6 +45,7 @@ class ViewController: UIViewController {
         
         webView = WKWebView(frame: view.frame, configuration: webViewConf)
         webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        webView.scrollView.keyboardDismissMode = .onDrag
         view.addSubview(webView)
         webView.navigationDelegate = self
         
@@ -50,16 +53,26 @@ class ViewController: UIViewController {
         webView.scrollView.addSubview(refreshControl)
         view.bringSubviewToFront(refreshControl)
         
-        load(url: "https://google.com")
+        load(url: baseURL)
     }
+    
+    // MARK: Buttons action's
     @IBAction func BackButtonAction(_ sender: Any) {
+        webView.goBack()
     }
     @IBAction func ForwardButtonAction(_ sender: Any) {
+        webView.goForward()
     }
     
     // MARK: Private methods
     private func load(url: String) {
-        webView.load(URLRequest(url: URL(string: url)!))
+        var urlToLoad: URL!
+        if let url = URL(string: url), UIApplication.shared.canOpenURL(url) {
+            urlToLoad = url
+        } else {
+            urlToLoad = URL(string: "\(baseURL)\(searchPath)\(url)")!
+        }
+        webView.load(URLRequest(url: urlToLoad))
     }
     
     @objc private func reload() {
@@ -72,17 +85,21 @@ class ViewController: UIViewController {
 extension ViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.endEditing(true)
+        load(url: searchBar.text ?? "")
     }
 }
 
 // MARK: WebView Navigation Delegate
 extension ViewController: WKNavigationDelegate {
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        backButton.isEnabled = webView.canGoBack
+        forwardButton.isEnabled = webView.canGoForward
         refreshControl.endRefreshing()
     }
     
     func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
         refreshControl.beginRefreshing()
+        searchBar.text = webView.url?.absoluteString
     }
 }
 
